c43cce7923cb3ff2d7ea7aefcf19dd7f8cfeaaa4
Ahammed Zayed Uddin Rahat <azurahat129@gmail.com>
2021-10-16 22:26:05 +0600

    add-features

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -6,26 +6,28 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert,
-} from "react-native";
+  Alert
+} from 'react-native';
 
-import Card from "../components/Card";
-import Input from "../components/Input";
-import NumberContainer from "../components/NumberContainer";
-import colors from "../constants/colors";
-import Colors from "../constants/colors";
+import Card from '../components/Card';
+import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import MainButton from '../components/MainButton';
+import Colors from '../constants/colors';
 
-const StartGameScreen = (props) => {
-  const [enteredValue, setEnteredValue] = useState("");
+const StartGameScreen = props => {
+  const [enteredValue, setEnteredValue] = useState('');
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
 
-  const numberInputHandler = (inputText) => {
-    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
 
   const resetInputHandler = () => {
-    setEnteredValue("");
+    setEnteredValue('');
     setConfirmed(false);
   };
 
@@ -33,15 +35,15 @@ const StartGameScreen = (props) => {
     const chosenNumber = parseInt(enteredValue);
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       Alert.alert(
-        "Invalid number!",
-        "Number has to be a number between 1 and 99.",
-        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
       );
       return;
     }
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
-    setEnteredValue("");
+    setEnteredValue('');
     Keyboard.dismiss();
   };
 
@@ -50,13 +52,11 @@ const StartGameScreen = (props) => {
   if (confirmed) {
     confirmedOutput = (
       <Card style={styles.summaryContainer}>
-        <Text>You selected</Text>
+        <BodyText>You selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button
-          title="START GAME"
-          color={Colors.primary}
-          onPress={() => props.onStartGame(selectedNumber)}
-        />
+        <MainButton onPress={() => props.onStartGame(selectedNumber)}>
+          START GAME
+        </MainButton>
       </Card>
     );
   }
@@ -68,9 +68,9 @@ const StartGameScreen = (props) => {
       }}
     >
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game!</Text>
+        <TitleText style={styles.title}>Start a New Game!</TitleText>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -108,38 +108,35 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: "center",
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
     marginVertical: 10,
-    color: Colors.text,
+    fontFamily: 'open-sans-bold'
   },
   inputContainer: {
     width: 300,
-    maxWidth: "80%",
-    alignItems: "center",
-    backgroundColor: colors.cardBg,
+    maxWidth: '80%',
+    alignItems: 'center'
   },
   buttonContainer: {
-    flexDirection: "row",
-    paddingTop: 10,
-    width: "100%",
-    justifyContent: "space-between",
-    paddingHorizontal: 15,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
   },
   button: {
-    width: 80,
+    width: 100
   },
   input: {
-    width: 80,
-    textAlign: "center",
+    width: 50,
+    textAlign: 'center'
   },
   summaryContainer: {
     marginTop: 20,
-    alignItems: "center",
-    backgroundColor: Colors.cardBg,
-  },
+    alignItems: 'center'
+  }
 });
 
 export default StartGameScreen;
